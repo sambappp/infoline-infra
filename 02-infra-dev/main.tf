1cfb1daae3702c5775b434815b2e9561fe6f8045
sambappp <skp@Mac.lan>
2026-03-18 13:35:01 +0100

    feat(create): create a resource group to store the tfstate of DEV infra

---
+++ 02-infra-dev/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.20.0"
+    }
+
+  }
+
+}
+
+provider "azurerm" {
+  subscription_id = "94d107f4-6183-4130-b847-11826e8b3cef"
+  features {}
+}
+
+module "rg_dev" {
+  source       = "../01-modules/01-rg-dev"
+  project_name = var.project_name
+  environment  = var.environment
+  location     = var.location
+}
+
+
